b086977532f8490c0faf14c9c23f0b6a668b4f34
AssouaAlbert <assouaalbert@gmail.com>
2019-08-12 23:53:13 +0100

    Spacial characters, Syntax, Comments,

--- Udemy Project/main.c
+++ Udemy Project/main.c
@@ -5,6 +5,12 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    /*
+    %s = strings
+    %d = intergers
+    %f = float
+     */
+    printf("Hello %s!, Thank you for signing up for this course %s\n", "Assoua","C Programming" );
+    printf("We will study %d projects and %1.2f will be the rating", 99,100.0);
     return 0;
 }
